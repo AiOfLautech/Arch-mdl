37bcd847c5fde4dc0092196b9c302fc550db044d
AiOfLautech <babalolahephzibah2@gmail.com>
2024-12-18 00:03:33 +0100

    Update config.js

--- config.js
+++ config.js
@@ -8,8 +8,8 @@ const chalk = require("chalk")
 const fs = require("fs")
 
 //======= Change settings =======\\
-global.owner = ["2347036214381"] // owner number 
-global.botname = ["Arch Md"] // don't change 
+global.owner = ["2348089336992"] // owner number 
+global.botname = ["Arch Md_Ai Of Lautech"] // don't change 
 global.simbol = "♘" // don't change 
 global.typeMenu = 'v1' //don't change 
 global.anticall = false // your choice 
